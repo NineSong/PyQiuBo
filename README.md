db843184197d2d590d8679d4dea1b06f45dc53f2
NineSong <etu951120@gmail.com>
2016-03-16 03:32:37 +0800

    Refactored some code

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ A Python script that helps students sign in DHU QiuBo app.
 `requests` can be installed through [`pip`](https://pypi.python.org/pypi/pip).
 With `pip` installed, execute `pip install requests` in a shell.
 
+# Usage
+Pass your student ID as a command-line parameter.
+
+e.g. If your student ID is 1234567890, execute in a shell
+```
+user ~ $ /path/to/QiuBo/qiubo.py 1234567890
+```
+
 # Log
 The script generates log file in its directory if there is any sign-in attempt.
 It simply consists of the time it tries to sign in and the JSON returned by the server.
-If the JSON contains `code:1`, it means that the attempt was successful.
+If the JSON contains `code: 1`, it means that the attempt was successful.
--- qiubo.py
+++ qiubo.py
@@ -2,59 +2,82 @@
 
 import requests
 import time
+import sys
 
-class SearchAllLessonId(object):
-    """docstring for SearchAllLessonId"""
+class SignInRobot(object):
+
     def __init__(self):
-        self.id = raw_input("Please enter your ID: ")
-        self.access_token = ''
-        # self.url = 'http://classair.dhu.edu.cn/mhs.php/Mhs/'
-        self.session = requests.Session()
+        self._id = sys.argv[1]
+        self._session = requests.Session()
+        self._access_token = self._get_token()
 
-    def Get_token(self):
-        password = self.id[-4:]
-        first_url = 'http://classair.dhu.edu.cn/mhs.php/Mhs/Login/doLogin?password=' + password + '&role=1&siteid=1&university_id=2008&username=' + self.id
-        first_page = self.session.get(first_url)
-        self.access_token = first_page.json()['results']['access_token']
+        if self._access_token is None:
+            print('Error: failed to log in!')
+            exit()
 
-    def Get_course_list(self):
-        _list = []
+    def _get_token(self):
+        response_json = self._session.get(
+            'http://classair.dhu.edu.cn/mhs.php/Mhs/Login/doLogin',
+            params={
+                'role': 1,
+                'siteid': 1,
+                'university_id': 2008,
+                'username': self._id,
+                'password': self._id[-4:]
+            }
+        ).json()
+
+        if 'results' in response_json:
+            return response_json['results']['access_token']
+        else:
+            return None
+
+    def get_class_schedule(self):
+        schedule = []
         _dict = {
             'day': '',
             'course_id': '',
             'lesson_id': '',
             'begin_time': '',
         }
-        self.Get_token()
-        Index_Url = 'http://classair.dhu.edu.cn/mhs.php/Mhs/Outline/index?access_token=' + self.access_token
-        re_index = requests.get(Index_Url)
-        for element in re_index.json()['results']['courseList']:
+
+        course_list = requests.get(
+            'http://classair.dhu.edu.cn/mhs.php/Mhs/Outline/index',
+            params={'access_token': self._access_token}
+        ).json()['results']['courseList']
+
+        for element in course_list:
             for _key in _dict:
                 _dict[_key] = element[_key]
-            _list.append(_dict.copy())
-        return _list
+            schedule.append(_dict.copy())
+        return schedule
 
-    def sign_in(self, _dict):
-        _sign_url = 'http://classair.dhu.edu.cn/mhs.php/Mhs/Online/signin/?'
-        _sign_url += 'course_id=' + _dict['course_id']
-        _sign_url += '&lesson_id=' + _dict['lesson_id']
-        _sign_url += '&stu_id=' + id
-        _text = self.session.get(_sign_url)
-        with open('log','a') as log:
+    def sign_in(self, course):
+        response = self._session.get(
+            'http://classair.dhu.edu.cn/mhs.php/Mhs/Online/signin/',
+            params={
+                'course_id': course['course_id'],
+                'lesson_id': course['lesson_id'],
+                'stu_id': self._id
+            }
+        )
+
+        with open('log', 'a') as log:
             _localtime = time.localtime(time.time())
-            log.write('month:' + str(_localtime.tm_mon))
-            log.write('day:' + str(_localtime.tm_mday))
-            log.write('hour:' + str(_localtime.tm_hour))
-            log.write('minute:' + str(_localtime.tm_min))
-            log.write('courseId:' + _dict['course_id'])
-            log.write(_text.text.encode('utf-8'))
-            log.write('\n')
+            log.write(
+                'month:' + str(_localtime.tm_mon) +
+                'day:' + str(_localtime.tm_mday) +
+                'hour:' + str(_localtime.tm_hour) +
+                'minute:' + str(_localtime.tm_min) +
+                'courseId:' + course['course_id'] +
+                response.text.encode('utf-8') + '\n'
+            )
         time.sleep(5400)
 
-class TimeControl(object):
-    """docstring for TimeControl"""
-    def __init__(self, _list):
-        self._list = _list
+class Timer(object):
+
+    def __init__(self, class_schedule):
+        self._list = class_schedule
         self._localtime = time.localtime(time.time())
         self._day = self._localtime.tm_wday + 1
         self._hour = self._localtime.tm_hour
@@ -66,7 +89,7 @@ class TimeControl(object):
         for element in self._list:
             _D_value = (element['day']-self._day) * 24 * 3600
             if _D_value >= 0:
-                _D_value += self.calculate_time(element['begin_time'])
+                _D_value += self._calculate_time(element['begin_time'])
                 if _D_value >= -5400:
                     break
                 else:
@@ -77,24 +100,28 @@ class TimeControl(object):
         self._sleep(element)
         return element
 
-    def calculate_time(self, begin_time):
+    def _calculate_time(self, begin_time):
         begin_time = begin_time.encode('utf-8')
         _D_hour = int(begin_time[:begin_time.index(':')]) - self._hour
         _D_min = int(begin_time[begin_time.index(':')+1:]) - self._min
         return (_D_hour*3600 + _D_min*60)
 
-    def _sleep(self,_dict):
+    def _sleep(self, _dict):
         _seconds = _dict['d_value']
         if _seconds > 0:
             print("Next course will start in %d minutes." % (_seconds / 60))
             time.sleep(_seconds)
 
 def main():
+    if len(sys.argv) != 2:
+        print('Usage: qiubo.py ID')
+        exit()
+
     while True:
-        S = SearchAllLessonId()
-        _list = S.Get_course_list()
-        T = TimeControl(_list)
-        S.sign_in(T.find_course())
+        robot = SignInRobot()
+        schedule = robot.get_class_schedule()
+        timer = Timer(schedule)
+        robot.sign_in(timer.find_course())
 
 if __name__ == '__main__':
     main()
